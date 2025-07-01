ecaf38b4a13476b63c0584815dd1101f8bdac15d
officialsanchi <nwangwudinma22@gmail.com>
2025-07-01 19:09:00 +0100

    linked my service interface to my service implementations

--- Backend/src/main/java/assessment/manager/data/repositories/UserRepo.java
+++ Backend/src/main/java/assessment/manager/data/repositories/UserRepo.java
@@ -2,9 +2,11 @@ package assessment.manager.data.repositories;
 
 import assessment.manager.data.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepo extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
 
