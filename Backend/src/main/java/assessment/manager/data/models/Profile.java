395181cbe524a51a64489b0cd5a5a1c33e3833ee
iam-fabulous <emmanuelebukainnah@gmail.com>
2025-07-02 11:57:25 +0100

    annotations added to profile class

--- Backend/src/main/java/assessment/manager/data/models/Profile.java
+++ Backend/src/main/java/assessment/manager/data/models/Profile.java
@@ -1,8 +1,12 @@
 package assessment.manager.data.models;
 
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.LocalDate;
 import java.util.List;
-
+@Document
+@Data
 public class Profile {
     private String userId;
     private String bio;
