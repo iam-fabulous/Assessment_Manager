74d0235f588d2f9c96dbef7714614f05438eaadc
Innah Emmanuel Chukwuebuka <emmanuelebukainnah@gmail.com>
2025-07-03 12:10:39 +0100

    Delete Frontend/assessment_management_system directory

--- Frontend/assessment_management_system/Component/LoginForm/Auth.ts
+++
@@ -1,7 +0,0 @@
-export const handleLogin = (email: string, password: string, role: string) => {
-  console.log('Logging in with:', email, password, role);
-};
-
-export const handleGoogleAuth = (role: string) => {
-  console.log('Authenticating with Google as:', role);
-};
